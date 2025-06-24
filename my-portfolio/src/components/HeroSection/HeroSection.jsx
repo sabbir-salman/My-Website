811a5d68df52de8d79bbb707e272c471a3b0f5b0
Sabbir Ahmed Salman <sabbir24salman@gmail.com>
2025-06-24 22:04:48 +0600

    add "download resume" button

--- my-portfolio/src/components/HeroSection/HeroSection.jsx
+++ my-portfolio/src/components/HeroSection/HeroSection.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import styles from "./HeroSection.module.css";
+import { Button } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
 
 export const HeroSection = () => {
   return (
     <div className={styles.heroSection}>
       <div className={`${styles.heroSectionName} container`}>
-        <h1> I’m Sabbir Ahmed salman </h1>
+        <h1> I’m Sabbir Ahmed Salman </h1>
       </div>
       <div className={`${styles.heroSectionDescription} container`}>
         <p> UX/UI designer. I design for user — not for myself. </p>
       </div>
-      <div className={`${styles.heroSectionButton} container`}>
-        <button className={styles.buttonPrimary}>Download CV</button>
+      <div className={styles.heroSectionButtonWrapper}>
+        <Button className={styles.heroSectionButton} size="large">
+          DOWNLOAD RESUME <DownloadOutlined />
+        </Button>
+
       </div>
-
-
     </div>
   );
 };
